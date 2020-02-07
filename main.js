ad8c5b2b82e68a6fae5c2e333de491e1a605feed
jeffreytram <jeffrey_tram@yahoo.com>
2020-02-06 21:09:36 -0500

    Added files

---
+++ main.js
@@ -0,0 +1,107 @@
+function render(dataset, category) {
+    if (category !== "") {
+        dataset = dataset.filter(d => d.Artist === category);
+    }
+
+    var svg = d3.select('svg');
+    //clear svg
+    svg.selectAll('*').remove();
+
+    //x-axis scale
+    var xScale = d3.scaleTime()
+        .domain(d3.extent(dataset, d => d.Time))
+        .nice()
+        .range([100, 500]);
+
+    //y-axis scale
+    var yScale = d3.scaleTime()
+        .domain([Date.now(), new Date("4/1/2018")])
+        .range([60, 660]);
+
+     //radius scale
+     //TODO: radius scale
+
+    //x-axis line
+    var xAxis = d3.axisBottom(xScale)
+        .ticks(d3.timeHour.every(2))
+        .tickFormat(d3.timeFormat('%H:%M'));
+
+    //y-axis line
+    var yAxis = d3.axisLeft(yScale)
+        .ticks(d3.timeMonth.every(1))
+        .tickFormat(d3.timeFormat('%b \'%y'));
+
+    //append x-axis
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,660)')
+        .call(xAxis);
+
+    //append x-axis label
+    svg.append('text')
+        .attr('class', 'x label')
+        .attr('transform', 'translate(250,720)')
+        .text('Time of Day (hrs:mins)');
+
+    //append y-axis
+    svg.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(100,0)')
+        .call(yAxis);
+
+    //append y-axis label
+    svg.append('text')
+        .attr('class', 'x label')
+        .attr('transform', 'translate(35,400) rotate(-90)')
+        .text('Date');
+
+    //append title to graph
+    svg.append('text')
+        .attr('class', 'title label')
+        .attr('transform', 'translate(60,40)')
+        .text('Jeffrey\'s Music Listening Times (4/2018 - 2/2020)');
+
+    //adding circles to svg
+    svg.selectAll('circle')
+        .data(dataset)
+        .enter()
+        .append('circle')
+        .attr('cx', d => xScale(d.Time))
+        .attr('cy', d => yScale(new Date(d.Date)))
+        .attr('r', 1)
+        .style('fill', 'white')
+        .style('opacity', .3);
+}
+
+//default view, no filter
+d3.csv('lastfm-data-utf.csv').then(dataset => {
+    //convert date string to data object
+    let newDate = new Date();
+    newDate.setHours(0, 0, 0, 0);
+    let newDateMilis = newDate.getTime();
+
+    dataset.forEach(d => {
+        var parts = d.Time.split(/:/);
+        var timePeriodMillis = (parseInt(parts[0], 10) * 60 * 60 * 1000) +
+            (parseInt(parts[1], 10) * 60 * 1000)
+        d.Time = new Date()
+        d.Time.setTime(newDateMilis + timePeriodMillis);
+    });
+
+    //render all data points
+    render(dataset, "");
+
+    //yorushika filter
+    let yorushikaButton = document.getElementById("yorushika");
+    yorushikaButton.addEventListener("click", function () {
+        render(dataset, "Yorushika");
+    });
+
+    //general filter event listener
+    let filterButton = document.getElementById("artist-filter-button");
+    filterButton.addEventListener("click", function () {
+        let artist = document.getElementById("artist-input").value;
+        console.log(artist);
+        render(dataset, artist);
+    });
+});
